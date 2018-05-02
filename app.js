8089bdabe9bdf3b7dc44fb90280d5a107c07bd8e
Zhemin_Su <Raymondsu2010@gmail.com>
2018-05-02 12:04:48 -0700

    update the middleware of the express

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var json = require('json');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var body_parser = require('body-parser');
 var app = express();
 
 // view engine setup
@@ -14,8 +14,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(body_parser());
+// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
